1536e420c5145496130995767167b2d1c2f9dc2f
JeonHyungJin <qpdjgjsl@gmail.com>
2018-07-30 12:17:08 +0900

    Firebase와 연동된 프로젝트 최초 commit

---
+++ app/src/main/java/allfor/project/a4to9/all_for/MainActivity.java
@@ -0,0 +1,13 @@
+package allfor.project.a4to9.all_for;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
